fd7d87b0d2211e7d90f84fbbaa260ed861bbfeee
Maxim Arbuzov <max.arbuzov@gmail.com>
2024-12-28 02:06:37 +0300

    Add no-loops binary insertion sort for sizes <= 16 minor formatting

--- small_sorts.h
+++ small_sorts.h
@@ -98,10 +98,11 @@ static __inline void INSERT_INTO_SORTED_6(SORT_TYPE *data, SORT_TYPE newItem) {
   }
 }
 
+// sorted size is 6 before call and 7 after
+// data[6] has no data before call
 #define INSERT_INTO_SORTED_7  SORT_MAKE_STR(insert_into_sorted_7)
 static __inline void INSERT_INTO_SORTED_7(SORT_TYPE *data, SORT_TYPE newItem) {
   SORT_TYPE item_3 = data[3];
-
   if (SORT_CMP(item_3, newItem) > 0) {
     data[6] = data[5];
     data[5] = data[4];
@@ -112,10 +113,11 @@ static __inline void INSERT_INTO_SORTED_7(SORT_TYPE *data, SORT_TYPE newItem) {
   }
 }
 
+// sorted size is 7 before call and 8 after
+// data[7] has no data before call
 #define INSERT_INTO_SORTED_8  SORT_MAKE_STR(insert_into_sorted_8)
 static __inline void INSERT_INTO_SORTED_8(SORT_TYPE *data, SORT_TYPE newItem) {
   SORT_TYPE item_3 = data[3];
-
   if (SORT_CMP(item_3, newItem) > 0) {
     data[7] = data[6];
     data[6] = data[5];
@@ -127,10 +129,11 @@ static __inline void INSERT_INTO_SORTED_8(SORT_TYPE *data, SORT_TYPE newItem) {
   }
 }
 
+// sorted size is 8 before call and 9 after
+// data[8] has no data before call
 #define INSERT_INTO_SORTED_9  SORT_MAKE_STR(insert_into_sorted_9)
 static __inline void INSERT_INTO_SORTED_9(SORT_TYPE *data, SORT_TYPE newItem) {
   SORT_TYPE item_4 = data[4];
-
   if (SORT_CMP(item_4, newItem) > 0) {
     data[8] = data[7];
     data[7] = data[6];
@@ -142,10 +145,11 @@ static __inline void INSERT_INTO_SORTED_9(SORT_TYPE *data, SORT_TYPE newItem) {
   }
 }
 
+// sorted size is 9 before call and 10 after
+// data[9] has no data before call
 #define INSERT_INTO_SORTED_10  SORT_MAKE_STR(insert_into_sorted_10)
 static __inline void INSERT_INTO_SORTED_10(SORT_TYPE *data, SORT_TYPE newItem) {
   SORT_TYPE item_4 = data[4];
-
   if (SORT_CMP(item_4, newItem) > 0) {
     data[9] = data[8];
     data[8] = data[7];
@@ -158,10 +162,11 @@ static __inline void INSERT_INTO_SORTED_10(SORT_TYPE *data, SORT_TYPE newItem) {
   }
 }
 
+// sorted size is 10 before call and 11 after
+// data[10] has no data before call
 #define INSERT_INTO_SORTED_11  SORT_MAKE_STR(insert_into_sorted_11)
 static __inline void INSERT_INTO_SORTED_11(SORT_TYPE *data, SORT_TYPE newItem) {
   SORT_TYPE item_5 = data[5];
-
   if (SORT_CMP(item_5, newItem) > 0) {
     data[10] = data[9];
     data[9] = data[8];
@@ -174,10 +179,11 @@ static __inline void INSERT_INTO_SORTED_11(SORT_TYPE *data, SORT_TYPE newItem) {
   }
 }
 
+// sorted size is 11 before call and 12 after
+// data[11] has no data before call
 #define INSERT_INTO_SORTED_12  SORT_MAKE_STR(insert_into_sorted_12)
 static __inline void INSERT_INTO_SORTED_12(SORT_TYPE *data, SORT_TYPE newItem) {
   SORT_TYPE item_5 = data[5];
-
   if (SORT_CMP(item_5, newItem) > 0) {
     data[11] = data[10];
     data[10] = data[9];
@@ -188,6 +194,82 @@ static __inline void INSERT_INTO_SORTED_12(SORT_TYPE *data, SORT_TYPE newItem) {
     INSERT_INTO_SORTED_6(data, newItem);
   } else {
     INSERT_INTO_SORTED_6(&data[6], newItem);
+  }
+}
+
+// sorted size is 12 before call and 13 after
+// data[12] has no data before call
+#define INSERT_INTO_SORTED_13  SORT_MAKE_STR(insert_into_sorted_13)
+static __inline void INSERT_INTO_SORTED_13(SORT_TYPE *data, SORT_TYPE newItem) {
+  SORT_TYPE item_6 = data[6];
+  if (SORT_CMP(item_6, newItem) > 0) {
+    data[12] = data[11];
+    data[11] = data[10];
+    data[10] = data[9];
+    data[9] = data[8];
+    data[8] = data[7];
+    data[7] = item_6;
+    INSERT_INTO_SORTED_7(data, newItem);
+  } else {
+    INSERT_INTO_SORTED_6(&data[7], newItem);
+  }
+}
+
+// sorted size is 13 before call and 14 after
+// data[13] has no data before call
+#define INSERT_INTO_SORTED_14  SORT_MAKE_STR(insert_into_sorted_14)
+static __inline void INSERT_INTO_SORTED_14(SORT_TYPE *data, SORT_TYPE newItem) {
+  SORT_TYPE item_6 = data[6];
+  if (SORT_CMP(item_6, newItem) > 0) {
+    data[13] = data[12];
+    data[12] = data[11];
+    data[11] = data[10];
+    data[10] = data[9];
+    data[9] = data[8];
+    data[8] = data[7];
+    data[7] = item_6;
+    INSERT_INTO_SORTED_7(data, newItem);
+  } else {
+    INSERT_INTO_SORTED_7(&data[7], newItem);
+  }
+}
+
+// sorted size is 14 before call and 15 after
+// data[14] has no data before call
+#define INSERT_INTO_SORTED_15  SORT_MAKE_STR(insert_into_sorted_15)
+static __inline void INSERT_INTO_SORTED_15(SORT_TYPE *data, SORT_TYPE newItem) {
+  SORT_TYPE item_7 = data[7];
+  if (SORT_CMP(item_7, newItem) > 0) {
+    data[14] = data[13];
+    data[13] = data[12];
+    data[12] = data[11];
+    data[11] = data[10];
+    data[10] = data[9];
+    data[9] = data[8];
+    data[8] = item_7;
+    INSERT_INTO_SORTED_8(data, newItem);
+  } else {
+    INSERT_INTO_SORTED_7(&data[8], newItem);
+  }
+}
+
+// sorted size is 15 before call and 16 after
+// data[15] has no data before call
+#define INSERT_INTO_SORTED_16  SORT_MAKE_STR(insert_into_sorted_16)
+static __inline void INSERT_INTO_SORTED_16(SORT_TYPE *data, SORT_TYPE newItem) {
+  SORT_TYPE item_7 = data[7];
+  if (SORT_CMP(item_7, newItem) > 0) {
+    data[15] = data[14];
+    data[14] = data[13];
+    data[13] = data[12];
+    data[12] = data[11];
+    data[11] = data[10];
+    data[10] = data[9];
+    data[9] = data[8];
+    data[8] = item_7;
+    INSERT_INTO_SORTED_8(data, newItem);
+  } else {
+    INSERT_INTO_SORTED_8(&data[8], newItem);
   }
 }
 
@@ -262,8 +344,32 @@ static __inline void INSERT_SORT_12(SORT_TYPE *data) {
   INSERT_INTO_SORTED_12(data, data[11]);
 }
 
+#define INSERT_SORT_13  SORT_MAKE_STR(insert_sort_13)
+static __inline void INSERT_SORT_13(SORT_TYPE *data) {
+  INSERT_SORT_12(data);
+  INSERT_INTO_SORTED_13(data, data[12]);
+}
+
+#define INSERT_SORT_14  SORT_MAKE_STR(insert_sort_14)
+static __inline void INSERT_SORT_14(SORT_TYPE *data) {
+  INSERT_SORT_13(data);
+  INSERT_INTO_SORTED_14(data, data[13]);
+}
+
+#define INSERT_SORT_15  SORT_MAKE_STR(insert_sort_15)
+static __inline void INSERT_SORT_15(SORT_TYPE *data) {
+  INSERT_SORT_14(data);
+  INSERT_INTO_SORTED_15(data, data[14]);
+}
+
+#define INSERT_SORT_16  SORT_MAKE_STR(insert_sort_16)
+static __inline void INSERT_SORT_16(SORT_TYPE *data) {
+  INSERT_SORT_15(data);
+  INSERT_INTO_SORTED_16(data, data[15]);
+}
+
 #define INSERT_SORT  SORT_MAKE_STR(insert_sort)
-// it is if-else only (no loops) binary insertion sort for size <= 12
+// it is if-else only (no loops) binary insertion sort for size <= 16
 SORT_DEF void INSERT_SORT(SORT_TYPE *data, const size_t size) {
   switch (size) {
     case 0:
@@ -314,9 +420,25 @@ SORT_DEF void INSERT_SORT(SORT_TYPE *data, const size_t size) {
       INSERT_SORT_12(data);
       break;
 
+    case 13:
+      INSERT_SORT_13(data);
+      break;
+
+    case 14:
+      INSERT_SORT_14(data);
+      break;
+
+    case 15:
+      INSERT_SORT_15(data);
+      break;
+
+    case 16:
+      INSERT_SORT_16(data);
+      break;
+
     default: {
-      INSERT_SORT_12(data);
-      BINARY_INSERTION_SORT_START(data, 12, size);
+      INSERT_SORT_16(data);
+      BINARY_INSERTION_SORT_START(data, 16, size);
     }
   }
 }
@@ -411,6 +533,10 @@ SORT_DEF void MERGE_SORT_SMALL_BALANCED(SORT_TYPE *data, const size_t size) {
 #undef INSERT_INTO_SORTED_10
 #undef INSERT_INTO_SORTED_11
 #undef INSERT_INTO_SORTED_12
+#undef INSERT_INTO_SORTED_13
+#undef INSERT_INTO_SORTED_14
+#undef INSERT_INTO_SORTED_15
+#undef INSERT_INTO_SORTED_16
 
 #undef INSERT_SORT_1
 #undef INSERT_SORT_2
@@ -424,6 +550,10 @@ SORT_DEF void MERGE_SORT_SMALL_BALANCED(SORT_TYPE *data, const size_t size) {
 #undef INSERT_SORT_10
 #undef INSERT_SORT_11
 #undef INSERT_SORT_12
+#undef INSERT_SORT_13
+#undef INSERT_SORT_14
+#undef INSERT_SORT_15
+#undef INSERT_SORT_16
 #undef INSERT_SORT
 
 #undef STABLE_PRESORT
